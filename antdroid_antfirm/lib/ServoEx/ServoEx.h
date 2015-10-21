6b0ca22cdf3b016ffe866598fff12678f9f83c5f
Pando85 <pando855@gmail.com>
2015-10-21 12:35:13 +0200

    Remove timer5 from library, and set 6 servos per timer

--- antdroid_antfirm/lib/ServoEx/ServoEx.h
+++ antdroid_antfirm/lib/ServoEx/ServoEx.h
@@ -79,11 +79,12 @@
 
 // Say which 16 bit timers can be used and in what order
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-#define _useTimer5
+//#define _useTimer5
 #define _useTimer1 
 #define _useTimer3
 #define _useTimer4 
-typedef enum { _timer5, _timer1, _timer3, _timer4, _Nbr_16timers } timer16_Sequence_t ;
+//typedef enum { _timer5, _timer1, _timer3, _timer4, _Nbr_16timers } timer16_Sequence_t ;
+typedef enum { _timer4, _timer1, _timer3, _Nbr_16timers } timer16_Sequence_t ;
 
 #elif defined(__AVR_ATmega32U4__)  
 #define _useTimer3
